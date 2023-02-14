c797aeb13c741722fd954687b3ed517015995831
MathieuCoding <polletmathieu.pro@gmail.com>
2023-02-14 20:39:39 +0100

    cards created

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const titre3 = <h1 className="text-center">Hello World of JSX !</h1>
 const Titre4 = (props) => <h1 className="text-center">{props.content}</h1> 
 
 
+// Création d'un composant boutton
 function MyButton()
 {
    return (
@@ -23,18 +24,48 @@ function MyButton()
    );
 }
 
+// Création d'un composant Produit
+const Product = (props) => 
+    <div className="card mt-3" style={{width: 18+'rem'}}>
+        <div className="card-bod text-center">
+            <img className="card-img-top" style={{height: 15+'rem'}} src={props.details.image} alt="Card image cap" />
+            <h5 className="card-title mt-3">{props.details.name}</h5>
+            <p className="card-text">{props.details.price}</p>
+            <a href="#" onClick={props.handleClick} className="btn btn-success mb-3">More details</a>
+        </div>
+    </div>
+
+
 // Composant dans une classe
-class Titre5 extends React.Component 
+class App extends React.Component 
 {
     state = {
-        content: "Mon titre dans le state"
+        products: [
+            {id: 1, name: "Cheeseburger", price: 5.95, image: "https:images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=815&q=80"},
+            {id: 2, name: "CBO", price: 8.95, image: "https://images.unsplash.com/photo-1615297928064-24977384d0da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80"},
+            {id: 3, name: "Filet-O-Fish", price: 3.95, image: "https://images.unsplash.com/photo-1520073201527-6b044ba2ca9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=412&q=80"}
+        ]
     }
+
+    handleClick = (e) => 
+    {
+        console.log(e.target.parentNode); // Renvoie la div entière
+    }
+
     render() 
     {
+        const productsList = this.state.products.map(product => 
+                <Product key={product.id} details={product} handleClick={this.handleClick}/>
+        )
+
         return( 
             <React.Fragment>
-                <h1 className="text-center">{this.state.content}</h1> 
+                <div className="container mt-5">
+                    {productsList}
+                </div>
+                <div className="container d-flex justify-content-center mt-5">
                 <MyButton />
+                </div>
             </React.Fragment>
         )
     }
@@ -42,14 +73,14 @@ class Titre5 extends React.Component
 
 
 // on rend l'élément dans le DOM en JS classique
-const app = document.querySelector('#app');
-app.appendChild(titre);
+const root = document.querySelector('#app');
+// root.appendChild(titre);
 
 
 // On rend l'élément dans le DOM via la méthode render() de ReactDOM
 ReactDOM.render(
-    <Titre5 />, 
-    app
+    <App />, 
+    root
 );
 
 
